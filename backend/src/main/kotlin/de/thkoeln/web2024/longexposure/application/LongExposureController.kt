efa526e38cffa4c2c4773a5b81ee6b56fdd290ce
denwae <43241874+denwae@users.noreply.github.com>
2024-09-20 21:26:29 +0200

    feat: implemented sseemitter removal via ping

--- backend/src/main/kotlin/de/thkoeln/web2024/longexposure/application/LongExposureController.kt
+++ backend/src/main/kotlin/de/thkoeln/web2024/longexposure/application/LongExposureController.kt
@@ -1,6 +1,7 @@
 package de.thkoeln.web2024.longexposure.application
 
 import jakarta.servlet.http.HttpServletResponse
+import jakarta.websocket.server.PathParam
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -23,16 +24,36 @@ class LongExposureController(
 
     private val emitters = mutableMapOf<UUID, SseEmitter>()
 
+    @PostMapping("/split-video/events/{emitterId}/ping", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun ssePing(
+        @PathVariable emitterId: UUID,
+        response: HttpServletResponse
+    ) {
+        response.setHeader("X-Accel-Buffering", "no")
+        LOGGER.info("Received ping for emitter with id $emitterId")
+        emitters[emitterId]?.send(
+            SseEmitter.event()
+                .name("ping")
+                .id("ping")
+                .data("Received")
+                .build()
+        ) ?: throw RuntimeException("No Emitter with this id")
+    }
+
     @PostMapping("/split-video/events", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     fun registerSplitVideoEventEmitter(
         @RequestParam(required = false) id: UUID?,
         response: HttpServletResponse
     ): SseEmitter {
         response.setHeader("X-Accel-Buffering", "no")
-        return SseEmitter(-1L).apply {
+        return SseEmitter(60000L).apply {
             val emitterId = UUID.randomUUID()
             LOGGER.info("Registered SSE Emitter with id: $emitterId")
             emitters[emitterId] = this
+            this.onCompletion{
+                emitters.remove(emitterId)
+                LOGGER.info("Removing SSE Emitter with id $emitterId")
+            }
             this.send(
                 SseEmitter.event()
                     .name("registered")
